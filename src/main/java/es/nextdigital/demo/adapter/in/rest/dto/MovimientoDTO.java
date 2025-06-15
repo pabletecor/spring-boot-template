66c446b52ff53d22bf210aef90d043032d764b02
pabletecor <pablocordonhidalgo@gmail.com>
2025-06-15 13:29:39 +0200

    FEAT: MovimientoDTO

--- src/main/java/es/nextdigital/demo/adapter/in/rest/dto/MovimientoDTO.java
+++ src/main/java/es/nextdigital/demo/adapter/in/rest/dto/MovimientoDTO.java
@@ -1,4 +1,14 @@
 package es.nextdigital.demo.adapter.in.rest.dto;
 
+import es.nextdigital.demo.application.models.enums.TipoMovimiento;
+import lombok.Data;
+
+
+@Data
 public class MovimientoDTO {
+    private Long id;
+    private Double cantidad;
+    private String descripcion;
+    private TipoMovimiento tipo;
+    private String fecha;
 }
